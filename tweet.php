32d5ba17a8fd76341118375b615cc0f00a640582
Xzevs <rasmus.karlsson@elev.ga.ntig.se>
2019-09-26 10:55:53 +0200

    fixade hemsida med ikoner

--- tweet.php
+++ tweet.php
@@ -10,12 +10,18 @@ $tweetId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 include 'include/dbinfo.php';
 include 'include/db.php';
 
+
 $sth = $dbh->prepare('SELECT * FROM tweet
             JOIN users
-            ON tweet.user_id = users.id
+            ON tweet.user_id = $users.id
             WHERE tweet.id = . $tweetId');
 $sth->execute();
 $row = $sth->fetch(PDO::FETCH_ASSOC);
 
-include 'views';
+echo "hejsan";
+
+print_r($row,1);
+
+include 'views/tweet.php';
+
 ?>
--- views/card.php
+++ views/card.php
@@ -1,13 +1,19 @@
 <section class="card mt-2">
-    <header class="card-header d-flex align-items-center">
-        <h5 class="card-title pl-4 mb-0">
-            <?= $row['name'] ?>
-            <small class="pl-4">
-                <?= $row['created_at'] ?>
-            </small>
-        </h5>
-    </header>
-    <a href="tweet.php?id=<?= $row['id'] ?>">
+    <a class="text-secondary" href="user.php?id=<?= $row['user_id'] ?>">
+        <header class="card-header d-flex align-items-center">
+            <img class="rounded-circle"
+                style="width:40px; height:40px;"
+                src="https://robohash.org/<?= $row['name'] ?>"
+                alt="Profile picture">
+            <h5 class="card-title pl-4 mb-0">
+                <?= $row['name'] ?>
+                <small class="pl-4">
+                    <?= $row['created_at'] ?>
+                </small>
+            </h5>
+        </header>
+    </a>
+    <a class="text-dark" href="tweet.php?id=<?= $row['id'] ?>">
         <div class="card-body">
             <p class="card-text">
                 <?= $row['body'] ?>
@@ -15,5 +21,6 @@
         </div>
     </a>
     <footer class="card-footer d-flex justify-content-end">
+        <i class="material-icons"></i>
     </footer>
 </section>
